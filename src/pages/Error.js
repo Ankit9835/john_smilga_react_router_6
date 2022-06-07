c8214ef7480bc8f301f71ad91cb90414d08a994f
ankit9835 <ankydchrismatic@gmail.com>
2022-06-07 22:24:11 +0530

    new commit

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,13 @@
+import { Link } from 'react-router-dom'
+
 const Error = () => {
   return (
     <section className='section'>
-      <h2>Error</h2>
+      <h2>404 Page Not Found</h2>
+      <Link to='/' className='btn'>
+        Back To Home
+      </Link>
     </section>
-  );
-};
-export default Error;
+  )
+}
+export default Error
