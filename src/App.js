a572fb60e797416fe74837b6ca8f14360351d66b
ankit9835 <ankydchrismatic@gmail.com>
2022-06-08 11:03:53 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Product from './pages/Products'
 import Error from './pages/Error'
+import Sharedlayout from './pages/SharedLayout'
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>our navbar</nav>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='about' element={<About />} />
-        <Route path='products' element={<Product />} />
-        <Route path='*' element={<Error />} />
+        <Route path='/' element={<Sharedlayout />}>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='products' element={<Product />} />
+          <Route path='*' element={<Error />} />
+        </Route>
       </Routes>
-      <footer>our footer</footer>
     </BrowserRouter>
   )
 }
